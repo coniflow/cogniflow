88b197a378f96e2134b6190bb8ff20aa70dc000b
coniflow <hassane.ahmane67@gmail.com>
2026-05-15 20:56:57 +0100

    Try with empty basePath

--- website/next.config.js
+++ website/next.config.js
@@ -5,8 +5,8 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  basePath: '/cogniflow',
-  assetPrefix: '/cogniflow/',
+  basePath: '',
+  assetPrefix: '',
   trailingSlash: true,
 };
 
